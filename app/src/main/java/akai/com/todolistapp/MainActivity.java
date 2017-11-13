30bf4af06a4396a667e63e17d858f88082af84c9
HenryTy <33482377+HenryTy@users.noreply.github.com>
2017-11-13 19:36:23 +0100

    Added AddTaskActivity

--- app/src/main/java/akai/com/todolistapp/MainActivity.java
+++ app/src/main/java/akai/com/todolistapp/MainActivity.java
@@ -1,7 +1,9 @@
 package akai.com.todolistapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openAddTaskActivity(View view){
+        Intent intent = new Intent(this, AddTaskActivity.class);
+        startActivity(intent);
     }
 }
